29a74a809fca667a1e1293be57a384a4dda21953
Pavel Lisitsyn <pavel.lisitsyn@hiq.ru>
2022-08-13 18:33:51 +0300

    updated riuting

--- ad-client/src/app/app-routing.module.ts
+++ ad-client/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NewsDetailComponent } from './main/news-detail/news-detail.component';
+import { NewsListComponent } from './main/news-list/news-list.component';
+import { ContentComponent } from './shared/components/global-layout/content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ContentComponent,
+    children: [
+      { path: '', redirectTo: '/news', pathMatch: 'full' },
+      { path: 'news', component: NewsListComponent },
+      {
+        path: 'news/item/:url',
+        component: NewsDetailComponent,
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
